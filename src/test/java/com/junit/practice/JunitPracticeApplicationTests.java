d6889a7bfb21d002332e2f0585cee1f13e5abc99
dev1week <rondo2860@gmail.com>
2024-07-27 23:05:40 +0900

    Initial commit

---
+++ src/test/java/com/junit/practice/JunitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junit.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
